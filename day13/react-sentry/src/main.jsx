2691e6087ddee2601e295b2925c91620138f5517
JamieLee0510 <walker200665@gmail.com>
2024-09-21 14:55:28 +0800

    fix: react code

--- day13/react-sentry/src/main.jsx
+++ day13/react-sentry/src/main.jsx
@@ -22,13 +22,3 @@ createRoot(document.getElementById('root')).render(
         <App />
     </StrictMode>,
 );
-Headers({
-    accept: 'application/json, text/plain, */*',
-    'user-agent': 'axios/1.7.7',
-    'accept-encoding': 'gzip, compress, deflate, br',
-    'sentry-trace': '282bec999ac14ac88f92c7c4bfa41f16-83a9c1fa172aa41d-1',
-    baggage:
-        'sentry-environment=production,sentry-public_key=fdcf31e5f9b89c2086c878ba04caada9,sentry-trace_id=282bec999ac14ac88f92c7c4bfa41f16,sentry-sample_rate=1,sentry-sampled=true',
-    host: 'localhost:3040',
-    connection: 'keep-alive',
-});
