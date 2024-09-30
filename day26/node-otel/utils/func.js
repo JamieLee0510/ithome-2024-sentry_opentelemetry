722d9b2c5bea806aa4c4fe44f16ce026505b51f0
JamieLee0510 <walker200665@gmail.com>
2024-09-30 11:53:12 +0800

    feat: day26---react+otel

---
+++ day26/node-otel/utils/func.js
@@ -0,0 +1,10 @@
+const sleep = (ms) =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
+
+module.exports = {
+    sleep,
+};
---
+++ day26/react-otel/src/App.jsx
@@ -0,0 +1,11 @@
+import * as axios from 'axios';
+import './App.css';
+import Demo from './components/Demo';
+
+function App() {
+    // initOtel();
+
+    return <Demo />;
+}
+
+export default App;
